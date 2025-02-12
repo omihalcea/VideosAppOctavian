aee1fddc75d74d9e387078aff80d2df3ba908d05
omihalcea <omihalcea@iesebre.com>
2025-02-12 21:15:56 +0100

    Sprint 3 acabat

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,17 +23,13 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         // Registrar les polítiques d'autorització
-        Gate::define('super-admin', function (User $user) {
+        Gate::define('manage-users', function (User $user) {
             return $user->isSuperAdmin();
         });
 
         // Gate per a gestionar vídeos
         Gate::define('manage-videos', function (User $user) {
             return $user->hasRole('video-manager') || $user->hasRole('super-admin');
-        });
-
-        Gate::define('access-app', function (User $user) {
-            return true; // Permet que tots els usuaris accedeixin a l'aplicació
         });
     }
 }
